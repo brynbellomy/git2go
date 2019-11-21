d3f4c293dfe64aa1e466cf966e9ca9330aecc27f
Bryn Bellomy <bryn.bellomy@gmail.com>
2019-11-21 16:54:29 -0600

    Action test

--- git_static.go
+++ git_static.go
@@ -4,9 +4,11 @@ package git
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/vendor/libgit2/include
-#cgo LDFLAGS: -L${SRCDIR}/vendor/libgit2/build/ -lgit2
-#cgo windows LDFLAGS: -lwinhttp
-#cgo !windows pkg-config: --static ${SRCDIR}/vendor/libgit2/build/libgit2.pc
+#cgo linux LDFLAGS: -L${SRCDIR}/libgit2-static/linux-latest -lgit2
+#cgo linux pkg-config: --static ${SRCDIR}/libgit2-static/linux-latest/libgit2.pc
+#cgo darwin LDFLAGS: -L${SRCDIR}/libgit2-static/macos-latest -lgit2
+#cgo darwin pkg-config: --static ${SRCDIR}/libgit2-static/macos-latest/libgit2.pc
+#cgo windows LDFLAGS: -L${SRCDIR}/libgit2-static/darwin-latest -lgit2 -lwinhttp
 #include <git2.h>
 
 #if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 27
